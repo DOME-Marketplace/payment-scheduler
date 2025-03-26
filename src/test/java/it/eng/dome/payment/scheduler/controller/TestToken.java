6b32ebe496100d9cd49e903d46b57ed1e63c31cd
pasquy <pasquale.vitale@gmail.com>
2025-03-26 13:36:00 +0100

    payment-scheduler 0.0.8c

---
+++ src/test/java/it/eng/dome/payment/scheduler/controller/TestToken.java
@@ -0,0 +1,18 @@
+package it.eng.dome.payment.scheduler.controller;
+
+import it.eng.dome.payment.scheduler.config.LearCredentialMachineLoader;
+import it.eng.dome.payment.scheduler.config.PrivateKeyLoader;
+import it.eng.dome.payment.scheduler.service.M2MTokenService;
+
+public class TestToken {
+	
+	
+	public static void main(String[] args) {
+		LearCredentialMachineLoader learCredentialMachineLoader = new LearCredentialMachineLoader();
+		
+		PrivateKeyLoader privateKey = new PrivateKeyLoader();
+		M2MTokenService m2m = new M2MTokenService(privateKey);
+		System.out.println(m2m.getAssertion(learCredentialMachineLoader.getLearCredentialMachine()).get("client_assertion"));
+		
+	}
+}
